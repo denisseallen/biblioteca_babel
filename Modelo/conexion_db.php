926261bf18678b793805912ecf1a7751d66309bd
denisseallen <131221237+denisseallen@users.noreply.github.com>
2023-04-19 13:03:02 -0600

    Update conexion_db.php

--- Modelo/conexion_db.php
+++ Modelo/conexion_db.php
@@ -3,10 +3,10 @@ class Conexion
 {
     public static function conectar()
     {
-        DEFINE ('DB_USER', 'user_biblioteca');
+        DEFINE ('DB_USER', 'id20629908_user_biblioteca');
         DEFINE ('DB_PASSWORD', '-~b#L92-hOZ4jbj_');
         DEFINE ('DB_HOST', 'localhost');
-        DEFINE ('DB_NAME', 'biblioteca_babel');
+        DEFINE ('DB_NAME', 'id20629908_biblioteca_babel');
 
         $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
